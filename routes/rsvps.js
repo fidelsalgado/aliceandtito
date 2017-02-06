3458819b5322b865f62300b7a5a2dd6f2424f603
Fidel Salgado <fidels@stanford.edu>
2017-02-05 23:20:35 -0800

    adding to db

--- routes/rsvps.js
+++ routes/rsvps.js
@@ -15,6 +15,8 @@ router.post('/', function(req, res, next) {
     var rsvp = req.body.rsvpcheck;
     var name = req.body.name;
     var email = req.body.email;
+    var date = new Date().toLocaleString("en-US", {timeZone: "America/Los_Angeles"});
+    console.log("Date = " + date);
 
     if (!rsvp || !name || !email)
       message = "Sorry there was an error receiving your rsvp. Please make sure you " +
@@ -25,12 +27,14 @@ router.post('/', function(req, res, next) {
       pg.connect(process.env.DATABASE_URL, function(err, client) {
         if (err) throw err;
         console.log('Connected to postgres! Getting schemas...');
-
-        client
-          .query('SELECT * FROM rsvps;')
-          .on('row', function(row) {
-            console.log(JSON.stringify(row));
-          });
+        client.query({
+          text: "INSERT INTO rsvps (rsvp, name, email, date) VALUES ($1, $2, $3, $4)",
+          values: [rsvp, name, email, date]
+        }, function(err, result) {
+          console.log("Successfull insert!");
+          console.log("err = " + err);
+          console.log("result = " + result);
+        });
       });
     }
   }
